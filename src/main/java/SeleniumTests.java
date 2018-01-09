c4972df59b37c9d02ede48a013f61d1a61c3ea81
Evgeniia Ershova <evgeniia_ershova@epam.com>
2018-01-09 12:51:36 +0300

    use local driver

--- src/main/java/SeleniumTests.java
+++ src/main/java/SeleniumTests.java
@@ -1,30 +1,38 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.containsString;
 
 
 public class SeleniumTests {
+/*
+    RemoteWebDriver driver;
+    Capabilities chromeCapabilities = DesiredCapabilities.chrome();
+    Capabilities firefoxCapabilities = DesiredCapabilities.firefox();*/
 
-    private WebDriver driver;
-
+    WebDriver driver;
 
 
     @BeforeClass
-    public void getDriver() {
+    public void getDriver() throws MalformedURLException {
         String browser = System.getProperty("browser");
         if (browser.equals("chrome")) {
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+//            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeCapabilities);
         }
         if (browser.equals("firefox")) {
             WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+//            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefoxCapabilities);
         }
     }
 
@@ -32,12 +40,23 @@ public class SeleniumTests {
     @Test
     public void getGoogle() {
         driver.get("http://google.com");
+        System.out.println(driver.getCurrentUrl());
         assertThat(driver.getCurrentUrl(), containsString("google.ru"));
     }
 
+    @Test
+    public void getYandex() {
+        driver.get("http://google.com");
+        System.out.println(driver.getCurrentUrl());
+        assertThat(driver.getCurrentUrl(), containsString("yandex.ru"));
+    }
+
     @AfterClass
-    public void closeDriver() {
-        driver.close();
-        driver.quit();
+    public void closeBrowser() {
+        if (driver !=null)
+        {
+            driver.close();
+            driver.quit();
+        }
     }
 }
